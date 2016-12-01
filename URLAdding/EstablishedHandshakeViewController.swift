fa288750a2db005f7ad957767acd11cb35b7b14a
Jordan Stapinski <jstapins@andrew.cmu.edu>
2016-12-01 11:05:51 -0500

    Added redirect back to main table view

--- URLAdding/EstablishedHandshakeViewController.swift
+++ URLAdding/EstablishedHandshakeViewController.swift
@@ -12,18 +12,52 @@ class EstablishedHandshakeViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     var data: [Any] = []
+    var timeLeft: Int = 0
+    var timer: Timer? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        timeLeft = 3
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(EstablishedHandshakeViewController.countDownTick), userInfo: nil, repeats: true)
+//        while (x > 0) {
+//            label.text = String(x)
+//            label.reloadInputViews()
+//            sleep(1)
+//            x = x - 1
+//        }
+        label.text = String(timeLeft)
+    }
+    
+    func countDownTick(){
+        //if timeLeft = 0
+        print(timeLeft)
+        if (timeLeft == 0) {
+            timer!.invalidate()
+            timer=nil
+            performSegue(withIdentifier: "backToMenu", sender: "")
+        } else {
+            timeLeft = timeLeft - 1
+            label.text = String(timeLeft)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "backToMenu") {
+            let finalDestination = segue.destination as? FirstViewController
+            //finalDestination?.data = sender as! [Any]
+            finalDestination?.navigationItem.hidesBackButton = true
+
+        }
+    }
 
     /*
     // MARK: - Navigation
